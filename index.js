c7648f3b49611ab26a73b54d6235e16ffc520ed1
Sebastian <sebastian@mattersupply.co>
2018-01-12 15:14:35 -0500

    Fixing Horizontal padding for Android

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class PopContent extends Component{
 						let tipContent = [];
 						if(content instanceof Array){
 							content.forEach((item, index, arr) => {
-								if(index > 9){ 
+								if(index > 9){
 									return;
 								}
 								item && ( tipContent[index] = (<Text style={styles.tipContent} key={'tipContent' + index}>{item}</Text>) );
@@ -332,6 +332,7 @@ let styles = StyleSheet.create({
 	tipContent: {
 		fontSize: 16,
 		marginBottom: 5,
+		paddingHorizontal: 13,
 		textAlign: 'center',
 	},
 	line: {
